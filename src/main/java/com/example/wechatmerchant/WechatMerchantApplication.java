4764944871554418e323f951aeff154635017432
Grootguo <genyuan.guo@gmail.com>
2024-05-21 09:41:16 +0800

    spring init

---
+++ src/main/java/com/example/wechatmerchant/WechatMerchantApplication.java
@@ -0,0 +1,13 @@
+package com.example.wechatmerchant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatMerchantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatMerchantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wechatmerchant/WechatMerchantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wechatmerchant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatMerchantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
