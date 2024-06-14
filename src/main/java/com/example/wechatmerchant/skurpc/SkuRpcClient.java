e5b5bc65a34a078804ed1f05c268e34536628189
Grootguo <genyuan.guo@gmail.com>
2024-06-14 22:33:00 +0800

    modify rpc init client

--- src/main/java/com/example/wechatmerchant/skurpc/SkuRpcClient.java
+++ src/main/java/com/example/wechatmerchant/skurpc/SkuRpcClient.java
@@ -4,7 +4,6 @@ import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.bootstrap.encrypt.EncryptionBootstrapConfiguration;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
@@ -19,26 +18,38 @@ public class SkuRpcClient {
     @Value("${sku-rpc-service-name:wechant-merchant-go}")
     private String skuRpcServiceName;
 
-    private final SkuServiceGrpc.SkuServiceBlockingStub blockingStub;
-
-    private final SkuInventoryServiceGrpc.SkuInventoryServiceBlockingStub blockingStubInventory;
+    private final DiscoveryClient discoveryClient;
 
     @Autowired
     public SkuRpcClient(DiscoveryClient  discoveryClient) {
+        this.discoveryClient = discoveryClient;
+    }
+
+    SkuServiceGrpc.SkuServiceBlockingStub getBlockingStub() {
         List<ServiceInstance> instances = discoveryClient.getInstances(this.skuRpcServiceName);
         ManagedChannel channel = ManagedChannelBuilder.forAddress(
                 instances.get(0).getHost(),
                 instances.get(0).getPort()
         ).usePlaintext().build();
 
-        this.blockingStub = SkuServiceGrpc.newBlockingStub(channel);
-        this.blockingStubInventory = SkuInventoryServiceGrpc.newBlockingStub(channel);
+        return SkuServiceGrpc.newBlockingStub(channel);
+    }
+
+    SkuInventoryServiceGrpc.SkuInventoryServiceBlockingStub getBlockingStubInventory() {
+        List<ServiceInstance> instances = discoveryClient.getInstances(this.skuRpcServiceName);
+        ManagedChannel channel = ManagedChannelBuilder.forAddress(
+                instances.get(0).getHost(),
+                instances.get(0).getPort()
+        ).usePlaintext().build();
+
+        return SkuInventoryServiceGrpc.newBlockingStub(channel);
     }
 
     SkuResp getSkuInfo(List<String> SkuIdList) throws Exception {
 
         SkuReq.Builder skuReq = SkuReq.newBuilder();
         // todo build sku id list
+        SkuServiceGrpc.SkuServiceBlockingStub blockingStub = getBlockingStub();
         SkuResp rsp = blockingStub.getSkuInfo(skuReq.build());
         if (rsp == null) {
             throw new Exception("sss");
@@ -58,6 +69,7 @@ public class SkuRpcClient {
         skuInventoryReq.addAllSkuId(SkuIdList);
         SkuInventoryReq request = skuInventoryReq.build();
         try {
+            SkuInventoryServiceGrpc.SkuInventoryServiceBlockingStub blockingStubInventory = getBlockingStubInventory();
             SkuInventoryResp resp = blockingStubInventory.getSkuInventoryInfo(request);
             if (resp == null) {
                 throw new Exception("sss");
